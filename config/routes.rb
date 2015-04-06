16a3cdf5ba0d7280ff7ae6f56c31e0f0f790f675
nsuitter <nsuitter@saleforce.com>
2015-04-05 22:57:28 -0700

    Updating app with langage option in footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
-  get 'about' => "static_pages#about"
+
+  scope "(:locale)" do
+    root :to => "static_pages#home"
+    get 'about' => "static_pages#about"
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
