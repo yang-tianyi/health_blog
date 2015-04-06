a91bc7ce087eaf2091b2463bcaf98aa77478f793
nsuitter <nsuitter@saleforce.com>
2015-04-06 09:43:11 -0700

    Adding new blog page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   scope "(:locale)" do
     root :to => "static_pages#home"
     get 'about' => "static_pages#about"
+    get 'blog' => "static_pages#blog"
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
